a99d6a378050ab415e7cc743020c2eb9429c97bc
jfpanchi <jefpanchi@gmail.com>
2025-01-09 10:14:57 -0500

    feat: :lipstick: Add config tailwind init
    Add config tailwind init wirh daisy-ui theme corporate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,30 +9,32 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center items-center p-4">
         <a href="#">
-          <img src={iUpiLogo} className="logo" alt="iUpi logo" />
+          <img src={iUpiLogo} className="logo h-16" alt="iUpi logo" />
         </a>
       </div>
-      <div>
+      <div className="flex justify-center gap-8 p-4">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo h-16" alt="Vite logo" />
         </a>
-
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo h-16" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-3xl font-bold text-center mt-4">Vite + React</h1>
+      <div className="card mt-6 p-4 bg-white rounded-lg shadow-md max-w-xs mx-auto">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="px-4 py-2 btn "
+        >
           count is {count}
         </button>
-        <p>
+        <p className="mt-2 text-center text-gray-600">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs mt-4 text-center text-blue-600">
         Click on the Vite and React logos to learn more
       </p>
     </>
