cbe41e3d50ddd172704ae4754f6d949b433c988e
jfpanchi <jefpanchi@gmail.com>
2025-01-11 11:11:16 -0500

    feat: 💄  Add tailwind theme default
    Add tailwind theme mytheme default

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,22 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["corporate"],
+    themes: [
+      {
+        mytheme: {          
+          primary: "#5E5F60",
+          secondary: "#A0A1A3",
+          accent: "#FFFFFF",
+          neutral: "#D1D2D3",
+          "base-100": "#FFFFFF",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E5E5",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
   },
 };
