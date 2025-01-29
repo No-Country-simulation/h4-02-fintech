e7e17bb004b8607f93fd9fc050cfd90216509eca
HikingCarrot7 <ricardoibarra2044@gmail.com>
2025-01-29 15:03:45 -0600

    fix: check response when requesting etfs

--- server/h4-02/h4-02/src/main/java/com/fintech/h4_02/service/ExchangeService.java
+++ server/h4-02/h4-02/src/main/java/com/fintech/h4_02/service/ExchangeService.java
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fintech.h4_02.dto.coin.CoinDto;
+import com.fintech.h4_02.dto.coin.CoinDtoRequest;
 import com.fintech.h4_02.dto.coin.CoinPrice;
 import com.fintech.h4_02.dto.exchange.ExchangeResponse;
 import com.fintech.h4_02.dto.exchange.ExchangeRrequest;
-import com.fintech.h4_02.dto.coin.CoinDtoRequest;
 import com.fintech.h4_02.dto.exchange.ExchangeSimple;
 import com.fintech.h4_02.dto.exchange.GetCoinByDatesRequest;
 import com.fintech.h4_02.entity.ExchangeEntity;
@@ -16,63 +15,32 @@ import com.fintech.h4_02.entity.UserEntity;
 import com.fintech.h4_02.enums.Coin;
 import com.fintech.h4_02.enums.State;
 import com.fintech.h4_02.exception.EntityNotFoundException;
+import com.fintech.h4_02.repository.ExchangeRepository;
 import com.fintech.h4_02.repository.UserRepository;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ExchangeService {
-
-    @Autowired
-    private com.fintech.h4_02.repository.ExchangeRepository exchangeRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
-    private ObjectMapper mapper;
-
+    private final ExchangeRepository exchangeRepository;
+    private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper mapper;
     private final String apikey = "2e2212a9163545fe83aee5773eb1639b";
-
-    public List<CoinDto> listCoinAll(Coin coin) throws ParserConfigurationException {
-
-
-        final String API_URL = "https://api.twelvedata.com/symbol_search?symbol_type=" + coin + "&exchange=BUENOSAIRES&apikey=9c41e2e67bc44ce180fcced64b41ea11";
-        try {
-            // Obtener la respuesta como String
-            String jsonResponse = restTemplate.getForObject(API_URL, String.class);
-
-            // Parsear la respuesta y extraer el nodo "data"
-            JsonNode rootNode = mapper.readTree(jsonResponse);
-            JsonNode dataNode = rootNode.get("data");
-
-            // Deserializar el nodo "data" como una lista de CoinDto
-            List<CoinDto> list = mapper.readValue(dataNode.toString(), new TypeReference<List<CoinDto>>() {
-            });
-
-            return list;
-        } catch (Exception e) {
-            throw new ParserConfigurationException("Parcer error");
-        }
-    }
 
     public List<CoinDtoRequest> listCoinAllForex(Coin coin) throws JsonProcessingException, JSONException {
         String url = null;
@@ -92,90 +60,78 @@ public class ExchangeService {
 
     private List<CoinDtoRequest> listBond() {
         List<String> listString = List.of("AAPL", "GOOGL", "MSFT", "AMZN", "TSLA", "FB", "NFLX", "NVDA", "BABA", "V");
-        List<CoinDtoRequest> list = listString.stream().map(CoinDtoRequest::new).toList();
-        return list;
+        return listString.stream().map(CoinDtoRequest::new).toList();
     }
 
-
     private List<CoinDtoRequest> parcearDtoApi(final String API_URL) throws JsonProcessingException {
-
         // Obtener la respuesta como String
         String jsonResponse = restTemplate.getForObject(API_URL, String.class);
-
         // Parsear la respuesta y extraer el nodo "data"
         JsonNode rootNode = mapper.readTree(jsonResponse);
         JsonNode dataNode = rootNode.get("data");
-
         // Deserializar el nodo "data" como una lista de CoinDto
-        List<CoinDtoRequest> list = mapper.readValue(dataNode.toString(), new TypeReference<List<CoinDtoRequest>>() {
+        return mapper.readValue(dataNode.toString(), new TypeReference<>() {
         });
-
-        return list;
-
     }
 
-
-    private List<CoinDtoRequest> listarEtfs() throws JSONException, JsonProcessingException {
+    private List<CoinDtoRequest> listarEtfs() {
         final String etfs = "https://api.twelvedata.com/etfs/list?apikey=".concat(apikey).concat("&source=docs");
-        List<CoinDtoRequest> listCoin = new ArrayList<>();
-
-        try {
-            JsonNode jsonResponse = restTemplate.getForObject(etfs, JsonNode.class);
-
-            // Verificar si la respuesta contiene un error
-            if (jsonResponse.has("error")) {
-                throw new RestClientException("Error en la respuesta de la API: " + jsonResponse.get("error").asText());
-            }
-
-            // Procesar la respuesta
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode list = jsonResponse.get("result").get("list");
-
-            if (list.isArray()) {
-                for (JsonNode item : list) {
-                    String symbol = item.get("symbol").asText();
-                    CoinDtoRequest coin = new CoinDtoRequest(symbol);
-                    listCoin.add(coin);
-                }
-            }
-
-            return listCoin;
-        } catch (RestClientException e) {
-            // Manejar excepciones de la API
-            e.printStackTrace();
-            throw e;
+        JsonNode jsonResponse = restTemplate.getForObject(etfs, JsonNode.class);
+        if (jsonResponse == null) {
+            throw new RestClientException("Error fetching ETFs data from API");
         }
+        // Check if the response has an error
+        if (jsonResponse.has("error")) {
+            throw new RestClientException("Error en la respuesta de la API: " + jsonResponse.get("error").asText());
+        }
+        if (!jsonResponse.has("result")) {
+            return List.of();
+        }
+        JsonNode list = jsonResponse.get("result").get("list");
+        if (!list.isArray()) {
+            return List.of();
+        }
+        List<CoinDtoRequest> listCoin = new ArrayList<>();
+        for (JsonNode item : list) {
+            if (!item.has("symbol")) {
+                continue;
+            }
+            String symbol = item.get("symbol").asText();
+            CoinDtoRequest coin = new CoinDtoRequest(symbol);
+            listCoin.add(coin);
+        }
+        return listCoin;
     }
 
-    private JsonNode conectionApi(String url) throws JsonProcessingException {
-        String jsonResponse = restTemplate.getForObject(url, String.class);
-
-        JsonNode rootNode = mapper.readTree(jsonResponse);
-
-        return rootNode;
-    }
-
-    public JsonNode conectionPrice(String coin) throws JsonProcessingException {
-
+    public JsonNode connectionPrice(String coin) throws JsonProcessingException {
         final String url = "https://api.twelvedata.com/time_series?symbol="
-                .concat(coin).
-                concat("&interval=1min&date=last&outputsize=1&apikey=")
+                .concat(coin)
+                .concat("&interval=1min&date=last&outputsize=1&apikey=")
                 .concat(apikey);
-
-        return conectionApi(url);
+        return connectionApi(url);
     }
 
-    public JsonNode getDescription(String coin) throws JsonProcessingException {
+    public JsonNode getCoinDescription(String coin) throws JsonProcessingException {
         final String url = "https://api.twelvedata.com/profile?symbol="
-                .concat(coin).
-                concat("&apikey=")
+                .concat(coin)
+                .concat("&apikey=")
                 .concat(apikey);
+        return connectionApi(url);
+    }
 
-        return conectionApi(url);
+    public JsonNode getCoinByDates(GetCoinByDatesRequest getCoinByDatesRequest) throws JsonProcessingException {
+        final String url = "https://api.twelvedata.com/time_series?end_date="
+                .concat(getCoinByDatesRequest.end().toString())
+                .concat("&start_date=")
+                .concat(getCoinByDatesRequest.start().toString())
+                .concat("&symbol=")
+                .concat(getCoinByDatesRequest.coin())
+                .concat("&interval=1day&apikey=")
+                .concat(apikey);
+        return connectionApi(url);
     }
 
     public ExchangeResponse create(ExchangeRrequest exchangeRrequest) {
-
         final UserEntity user = userRepository.findById(exchangeRrequest.userId()).orElseThrow(() -> new EntityNotFoundException("user not found"));
         final BigDecimal value = new BigDecimal(exchangeRrequest.value());
         final BigDecimal total = value.multiply(BigDecimal.valueOf(exchangeRrequest.quantity()));
@@ -188,36 +144,27 @@ public class ExchangeService {
                 .quantity(exchangeRrequest.quantity())
                 .total(total)
                 .build();
-       final ExchangeEntity exchangeDb = exchangeRepository.save(exchange);
+        final ExchangeEntity exchangeDb = exchangeRepository.save(exchange);
         return new ExchangeResponse(exchangeDb);
     }
 
-    public List<ExchangeResponse> GetByUser(@NotNull Long id) {
-        UserEntity user = userRepository.findById(id).orElseThrow( ()-> new EntityNotFoundException("user not found"));
+    public List<ExchangeResponse> getByUser(@NotNull Long id) {
+        UserEntity user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("user not found"));
         List<ExchangeEntity> list = exchangeRepository.findAllByUserId(user);
-        List<ExchangeResponse> listDto = list.stream().map(ExchangeResponse::new).toList();
-        return listDto;
+        return list.stream().map(ExchangeResponse::new).toList();
     }
 
     public List<ExchangeSimple> getTotalCoinByUser(@NotNull Long id) {
-
         final UserEntity user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("user not found"));
-
-       List<Object> list = exchangeRepository.getTotalCoinByUser(user);
-        List<ExchangeSimple> dtoList =list.stream().map(ExchangeSimple::fromObjectList)
-                        .collect(Collectors.toList());
-
-                return dtoList;
-
+        List<Object> list = exchangeRepository.getTotalCoinByUser(user);
+        return list.stream().map(ExchangeSimple::fromObjectList).toList();
     }
 
     public ExchangeResponse sell(ExchangeRrequest exchangeRrequest) {
         final UserEntity user = userRepository.findById(exchangeRrequest.userId()).orElseThrow(() -> new EntityNotFoundException("user not found"));
         final BigDecimal value = new BigDecimal(exchangeRrequest.value());
         final BigDecimal total = value.multiply(BigDecimal.valueOf(exchangeRrequest.quantity()));
-
-        ferificarCoinStock(user,exchangeRrequest.coin(),exchangeRrequest.quantity());
-
+        checkUserCoinStock(user, exchangeRrequest.coin(), exchangeRrequest.quantity());
         ExchangeEntity exchange = ExchangeEntity.builder()
                 .value(value)
                 .date(LocalDate.now())
@@ -231,31 +178,16 @@ public class ExchangeService {
         return new ExchangeResponse(exchangeDb);
     }
 
-    private void ferificarCoinStock(@NotNull UserEntity user, @NotBlank String coin,@NotBlank int quantity) {
-
+    private void checkUserCoinStock(@NotNull UserEntity user, @NotBlank String coin, @NotBlank int quantity) {
         List<Object> list = exchangeRepository.getTotalCoinByUser(user);
-
-        List<ExchangeSimple> dtoList =list.stream().map(ExchangeSimple::fromObjectList).toList();
-
+        List<ExchangeSimple> dtoList = list.stream().map(ExchangeSimple::fromObjectList).toList();
         Optional<ExchangeSimple> exchangeSimple = dtoList.stream().filter(d -> d.coin().equals(coin)).findFirst();
-
-        if(exchangeSimple.isEmpty()) throw new RuntimeException("no tienes ".concat(coin.toString()).concat("en tu cuenta"));
-
-        if (exchangeSimple.get().total() < quantity) throw new RuntimeException("no tienes suficiente cantidad de".concat(coin.toString()));
-    }
-
-
-    public JsonNode getCoinByDates(GetCoinByDatesRequest getCoinByDatesRequest) throws JsonProcessingException {
-        final String url = "https://api.twelvedata.com/time_series?end_date="
-                .concat(getCoinByDatesRequest.end().toString()).
-                concat("&start_date=")
-                .concat(getCoinByDatesRequest.start().toString())
-                .concat("&symbol=")
-                .concat(getCoinByDatesRequest.coin())
-                .concat("&interval=1day&apikey=")
-                .concat(apikey);
-
-        return conectionApi(url);
+        if (exchangeSimple.isEmpty()) {
+            throw new RuntimeException("no tienes ".concat(coin).concat("en tu cuenta"));
+        }
+        if (exchangeSimple.get().total() < quantity) {
+            throw new RuntimeException("no tienes suficiente cantidad de".concat(coin));
+        }
     }
 
     public List<CoinPrice> findPriceCoins(UserEntity user) {
@@ -264,15 +196,13 @@ public class ExchangeService {
 
     public List<CoinPrice> findPriceSellCoins(List<ExchangeSimple> coins) throws JsonProcessingException, JSONException {
         List<CoinPrice> list = new ArrayList<>();
-        for(ExchangeSimple e : coins){
-            JsonNode  response  = conectionPrice(e.coin());
+        for (ExchangeSimple e : coins) {
+            JsonNode response = connectionPrice(e.coin());
             String value = getCloseValueFromApiResponse(String.valueOf(response));
             list.add(new CoinPrice(e.coin(), Double.valueOf(value)));
-
         }
         return list;
     }
-
 
     private String getCloseValueFromApiResponse(String jsonResponse) throws JSONException {
         JSONObject jsonObject = new JSONObject(jsonResponse);
@@ -283,4 +213,10 @@ public class ExchangeService {
         }
         return null;
     }
+
+    private JsonNode connectionApi(String url) throws JsonProcessingException {
+        String jsonResponse = restTemplate.getForObject(url, String.class);
+        return mapper.readTree(jsonResponse);
+    }
+
 }
