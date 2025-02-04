5700f46da944864c67233ab4c19d07066bce8640
lionel staricoff <staricofflionel@hotmail.com>
2025-02-04 15:03:54 -0500

    fix:  mapeando entidad QueryEntity con UserEntity

---
+++ server/h4-02/h4-02/src/main/java/com/fintech/h4_02/enums/QueriesState.java
@@ -0,0 +1,12 @@
+package com.fintech.h4_02.enums;
+
+public enum QueriesState {
+    QUERY("query"),
+    ERROR("error");
+
+    private final String value;
+
+    QueriesState(String value) {
+        this.value = value;
+    }
+}
